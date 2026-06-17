2e3e2892cb9b09a002aa12d8819c7b5acf4ad86d
abhilashkuvalkar <abhilashkuvalkar@live.com>
2026-06-17 22:46:58 +0530

    Updated Azure Redis Cache to Azure Managed Redis

--- infrastructure/terraform/modules/redis/main.tf
+++ infrastructure/terraform/modules/redis/main.tf
@@ -2,18 +2,15 @@ locals {
   redis_name = "flowdesk-redis-${var.environment_name}"
 }
 
-resource "azurerm_redis_cache" "this" {
-  name                 = local.redis_name
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  sku_name             = "Basic"
-  capacity             = 0
-  family               = "C"
-  minimum_tls_version  = "1.2"
-  non_ssl_port_enabled = false
+resource "azurerm_managed_redis" "this" {
+  name                  = local.redis_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  sku_name              = "Balanced_B50"
+  public_network_access = false
 
-  redis_configuration {
-    maxmemory_policy = "allkeys-lru"
+  identity {
+    type = "SystemAssigned"
   }
 
   lifecycle {
