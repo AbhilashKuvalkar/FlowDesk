821e42ed1193299cb86716ab00bfc47603e58d50
abhilashkuvalkar <abhilashkuvalkar@live.com>
2026-06-19 21:22:55 +0530

    Changed Azure Redis Cache to Azure Managed Redis and its related properties. Also, changes related TF version update

--- infrastructure/terraform/modules/redis/main.tf
+++ infrastructure/terraform/modules/redis/main.tf
@@ -7,7 +7,10 @@ resource "azurerm_managed_redis" "this" {
   resource_group_name   = var.resource_group_name
   location              = var.location
   sku_name              = "Balanced_B50"
-  public_network_access = false
+  public_network_access = "Disabled"
+  default_database {
+    access_keys_authentication_enabled = true
+  }
 
   identity {
     type = "SystemAssigned"
--- infrastructure/terraform/modules/redis/outputs.tf
+++ infrastructure/terraform/modules/redis/outputs.tf
@@ -1,11 +1,11 @@
 output "redis_hostname" {
-  value = azurerm_redis_cache.this.hostname
+  value = azurerm_managed_redis.this.hostname
 }
 
 output "redis_ssl_port" {
-  value = azurerm_redis_cache.this.ssl_port
+  value = azurerm_managed_redis.this.default_database[0].port
 }
 
 output "redis_primary_key" {
-    value = azurerm_redis_cache.this.primary_access_key
+  value = azurerm_managed_redis.this.default_database[0].primary_access_key
 }
